55d439b1fae58951b90d192333bcc1b83a6fc20c
steve <steve.kossouho@gmail.com>
2015-05-22 10:00:53 +0200

    A small bit of work.

---
+++ approval/util/moderator.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from __future__ import absolute_import
+
+
+class Moderator(object):
+    """ Moderator class, where you define moderation mechanisms for your models """
+    model = None  # Model class to moderate
+
+    # Getter
+    def auto_moderate(self, obj):
+        """
+        Automoderate an object of the moderator model                                                                                                           
+        :param obj: model instance to moderate
+        :type obj: django.db.models.Model
+        :returns: the moderation status for the object
+        :rtype: bool
+        """
+        return True
