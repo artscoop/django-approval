d9694d8056612f4bcd8d72855a55c6e5d43a0638
steve <steve.kossouho@gmail.com>
2015-09-23 21:19:38 +0200

    Python 3.2 fixes. Made better docstrings. Fixed monkey-patching of Model in Django 1.8+ and Python 3.2+

---
+++ approval/models/edits.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+"""
+Parametrized model containing edits.
+It allows one to have
+"""
+__author__ = 'steve'
--- approval/util/signals.py
+++ approval/util/signals.py
@@ -1,2 +1,10 @@
 # coding: utf-8
 """ Signals sent by the application """
+from django.dispatch.dispatcher import Signal
+
+
+# Sent just before an instance undergoes approval
+pre_approval = Signal(['instance', 'status'])
+
+# Sent just after an instance undergoes approval
+post_approval = Signal(['instance', 'status'])
