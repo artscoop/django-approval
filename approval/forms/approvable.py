e30b9761f0faab8f5b2ec54d6cfda0e7b9d822a9
artscoop <artscoop93.info@gmail.com>
2016-06-11 12:10:01 +0200

    [F] fixed method call parameter in approval.approvedmodel._revert

--- approval/forms/approvable.py
+++ approval/forms/approvable.py
@@ -7,6 +7,11 @@ class ApprovableForm():
     """ ModelForm mixin """
 
     def __init__(self, *args, **kwargs):
+        """
+        Form initializer for ApprovedModel
+
+        The form is initialized with the instance data fetched from the sandbox
+        """
         instance = kwargs.get('instance', None)
         if instance and isinstance(instance, ApprovedModel):
             instance.approval._update_source()
--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='approval',
-    version='0.3.20160211',
+    name='django-approval',
+    version='0.9.20160611',
     packages=find_packages('.'),
     include_package_data=True,
     url='',
-    author='Estelle Kossouho',
+    author='S Kossouho',
     author_email='artscoop93@gmail.com',
     description='Approval mechanisms for model instances in Django 1.8+',
     requires=['django', 'django_picklefield', 'unidecode', 'django_annoying'],
     classifiers=["License :: OSI Approved :: BSD License",
                  "Framework :: Django",
                  "Development Status :: 4 - Beta",
+                 "Programming Language :: Python :: 3"
                  ]
 
 )
